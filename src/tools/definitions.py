5e22ff2aaabd0795e43944004420282e45c53c57
ivzx04 <ivan.af.paus@gmail.com>
2026-08-12 13:52:22 +0200

    Feat: added a visit webpage tool

--- src/tools/definitions.py
+++ src/tools/definitions.py
@@ -3,7 +3,7 @@ from tools.calculator import CalculatorTool
 
 from core.registry import Registry
 
-from smolagents import  DuckDuckGoSearchTool, WikipediaSearchTool
+from smolagents import  DuckDuckGoSearchTool, WikipediaSearchTool, VisitWebpageTool
 
 # tool registry global object that will keep track of string -> factory mappings for building our tools
 tool_registry = Registry("Tool")
@@ -15,6 +15,11 @@ def make_web_search_tool(watcher) -> WrappedTool:
 @tool_registry.register("wikipedia_search")
 def make_wiki_search_tool(watcher) -> WrappedTool:
     return WrappedTool(wrapped_tool=WikipediaSearchTool(), watcher=watcher)
+
+
+@tool_registry.register("visit_webpage")
+def make_visit_webpage_tool(watcher) -> WrappedTool:
+    return WrappedTool(wrapped_tool=VisitWebpageTool(), watcher=watcher)
 
 @tool_registry.register("calculator")
 def make_calculator_tool(watcher) -> WrappedTool:
